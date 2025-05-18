ff4a9ed8dbc54383a8e46a823797ca6ee8cd6848
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-05-18 19:28:42 +0800

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,17 +1,14 @@
 
 import spanish from '@/i18n/zh.json';
-import english from '@/i18n/en.json';
 
 const LANG = {
   SPANISH: 'zh',
-  ENGLISH: 'en',
 };
 
 export const getI18N = (
   { currentLocale = 'zh' }:
     { currentLocale: string | undefined; }
 ) => {
-  if (currentLocale === LANG.ENGLISH) return english
-  if (currentLocale === LANG.SPANISH) return spanish
+  if (currentLocale === LANG.ENGLISH) return zhcn
   return spanish;
 };
