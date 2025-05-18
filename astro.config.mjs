5b56d43f4899cbddf2bae3683ee0189365b11b8a
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-05-18 19:10:35 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: 'zh',
+    locales: ['zh', 'en'],
     routing: {
       prefixDefaultLocale: false
     }
