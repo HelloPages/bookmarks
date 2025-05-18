c3e6b80b2ec76166e9b7afc88ee0796aeb38bd46
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-05-18 20:35:23 +0800

    Create shopnow.js

---
+++ src/data/shopnow.js
@@ -0,0 +1,72 @@
+export const cardsData = [
+  {
+    id: "car-1",
+    url: "www.tabler.io",
+    href: "https://tabler.io/icons",
+    title: "Tabler Icons",
+    subtitle: "826 pixel-perfect icons for web design.",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-2",
+    url: "www.skillicons.dev",
+    href: "https://skillicons.dev/",
+    title: "Skill Icons",
+    subtitle: "Showcase your skills on your GitHub or resumé with ease!",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-3",
+    url: "www.svgl.app",
+    href: "https://svgl.app/",
+    title: "SVGL App",
+    subtitle: "🧩 A beautiful library with SVG logos. Built with Sveltekit & Tailwind CSS.",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-4",
+    url: "www.shots.so",
+    href: "https://www.shots.so/",
+    title: "Shots",
+    subtitle: "Create Amazing Mockups",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-5",
+    url: "www.squoosh.app",
+    href: "https://squoosh.app/",
+    title: "Squoosh",
+    subtitle: "Squoosh is the ultimate image optimizer that allows you to compress and compare images.",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-6",
+    url: "www.bg.ibelick.com",
+    href: "https://bg.ibelick.com/",
+    title: "BG.IBELICK",
+    subtitle: "Modern, Ready-to-Use Background Snippets for Developers.",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+  {
+    id: "card-7",
+    url: "www.undraw.co",
+    href: "https://undraw.co/illustrations",
+    title: "unDraw - Open source illustrations for any idea",
+    subtitle: "Open-source illustrations for any idea you can imagine and create.",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Logo de Web",
+
+  },
+];
