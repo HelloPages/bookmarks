0ef69c5901c18b62fec96945b12b864eba5bccd8
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-05-18 19:14:19 +0800

    Update educationhub.js

--- src/data/educationhub.js
+++ src/data/educationhub.js
@@ -10,4 +10,14 @@ export const cardsData = [
 
   },
 
+  {
+    id: "card-2",
+    url: "fullstackopen.com",
+    href: "https://fullstackopen.com/",
+    title: "Full stack open",
+    subtitle: "Open online course on JavaScript based modern web development by University of Helsinki and Houston Inc..",
+    imageSrc: "/img/avatar.png",
+    imageAlt: "Ícono del aplicativo",
+
+  },
 ];
